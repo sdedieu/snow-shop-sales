7a74de44973c3169988573e7f29cc8d10228f2d9
sdedieu <dedieu.sylvain38@gmail.com>
2022-02-02 16:45:50 +0100

    add snow shop lib

--- src/app/sales/sale-details/sale-details.component.ts
+++ src/app/sales/sale-details/sale-details.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SnowShopLibService } from 'snow-shop-lib';
 
 @Component({
   selector: 'app-sale-details',
@@ -19,12 +20,18 @@ export class SaleDetailsComponent implements OnInit {
 
   sizeSelected = 0;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  constructor(private http: HttpClient, 
+    private route: ActivatedRoute, 
+    private snowShopLibService: SnowShopLibService) { }
 
   ngOnInit(): void {
     this.http.get('http://localhost:3000/sales/' + this.route.snapshot.params.saleId).subscribe(
       item => this.item = item
     )
+  }
+
+  shop(){
+    this.snowShopLibService.increment();
   }
 
 }
